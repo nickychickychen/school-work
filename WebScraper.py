d619462e140fea291588180288880bc1789a0fb9
Nick <72486686+nickychickychen@users.noreply.github.com>
2022-10-19 16:56:27 -0700

    Simple Web Scraper
    Uploaded code from a simple web scraper created as an AI Club project. This was meant to get practice using python libraries. Uses Selenium library and Yahoo Finance to get a CSV file of stock data for the stock NYSEARCA:SPY.

---
+++ WebScraper.py
@@ -0,0 +1,56 @@
+#wall of imports
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from time import sleep
+#import pandas as pd      # use this for analyzing data?
+#import pygsheets as ps   # use this to analyze data?
+
+#NOTE: timers may not work if ads are too chunky. just reload the script at this point
+#NOTE: see todo below
+
+
+PATH = "C:\Program Files (x86)\chromedriver.exe" #this is where I put my chromedriver; it's dif for everyone
+driver = webdriver.Chrome(PATH) 
+
+stockList = ['SPY'] #current test stock list is size 1. SPY is an ETF that tracks the S&P 500
+
+i = 0
+while i < len(stockList): #look up all the stonks
+    openYFi = driver.get('https://finance.yahoo.com/')
+    searchBox = driver.find_element_by_id('yfin-usr-qry') #this is what the search bar is called. found from f12 and inspecting element on the page.
+    searchBox.send_keys("{0}".format(stockList[i])) #send in current value of the stock list
+    time.sleep(3) #wait 3 secs for loading
+    stockPg = searchBox.send_keys(Keys.RETURN)
+    time.sleep(4) #wait 3 secs for loading
+    i+=1 
+
+#TODO: uhh i think the stuff below should be in the loop
+#whatever that's a prob for later
+
+histData = driver.find_element(By.XPATH, '//span[text()="Historical Data"]').click() #look for 1y hist. data
+time.sleep(3) 
+clickApply = driver.find_element(By.XPATH, '//span[text()="Apply"]').click() #confirm the time length
+
+downloadData = driver.find_element(By.XPATH, '//span[text()="Download"]').click() #download csv file of data
+time.sleep(5) #wait 5 secs for download.  this can change if the computer/wifi is bad
+driver.close() 
+i+=1 #if i move the above chunk into the while just delete this
+
+
+
+#TODO: now it downloads a csv file that only works in google sheets (sad pog)
+
+#next step: do something with the data lol
+
+#idea: use some ML algo to give a prediction on whether or not a stock would rise
+# based on daily percent changes
+
+#idea extension: also could give a predicted date the stock would rise/fall based
+# on old data? this would use backtesting with the downloaded historical data
+
+#help plz wut am i doing    
